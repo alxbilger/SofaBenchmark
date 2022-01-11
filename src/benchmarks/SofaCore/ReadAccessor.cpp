91faa609f25d82c05459e97bba08e4dde89abadd
Alex Bilger <alexbilger0@gmail.com>
2022-01-11 11:04:29 +0100

    All types of conversion

--- src/benchmarks/SofaCore/ReadAccessor.cpp
+++ src/benchmarks/SofaCore/ReadAccessor.cpp
@@ -6,15 +6,29 @@ template<class T>
 static void BM_Data_GetValue(benchmark::State& state);
 
 template<class T>
-static void BM_Data_ReadAccessor(benchmark::State& state);
+static void BM_Data_ReadAccessor_ImplicitConversion(benchmark::State& state);
+
+template<class T>
+static void BM_Data_ReadAccessor_ExplicitConversion(benchmark::State& state);
+
+template<class T>
+static void BM_Data_ReadAccessor_StarOperatorConversion(benchmark::State& state);
 
 BENCHMARK_TEMPLATE(BM_Data_GetValue, int);
 BENCHMARK_TEMPLATE(BM_Data_GetValue, sofa::type::vector<int>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
 BENCHMARK_TEMPLATE(BM_Data_GetValue, sofa::type::vector<sofa::type::Vec3>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
 
-BENCHMARK_TEMPLATE(BM_Data_ReadAccessor, int);
-BENCHMARK_TEMPLATE(BM_Data_ReadAccessor, sofa::type::vector<int>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
-BENCHMARK_TEMPLATE(BM_Data_ReadAccessor, sofa::type::vector<sofa::type::Vec3>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_ImplicitConversion, int);
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_ImplicitConversion, sofa::type::vector<int>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_ImplicitConversion, sofa::type::vector<sofa::type::Vec3>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
+
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_ExplicitConversion, int);
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_ExplicitConversion, sofa::type::vector<int>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_ExplicitConversion, sofa::type::vector<sofa::type::Vec3>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
+
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_StarOperatorConversion, int);
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_StarOperatorConversion, sofa::type::vector<int>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
+BENCHMARK_TEMPLATE(BM_Data_ReadAccessor_StarOperatorConversion, sofa::type::vector<sofa::type::Vec3>)->RangeMultiplier(10)->Ranges({{1e4, 1e6}});
 
 template<class T>
 void BM_Data_GetValue(benchmark::State& state)
@@ -35,7 +49,7 @@ void BM_Data_GetValue(benchmark::State& state)
 }
 
 template<class T>
-void BM_Data_ReadAccessor(benchmark::State& state)
+void BM_Data_ReadAccessor_ImplicitConversion(benchmark::State& state)
 {
     sofa::core::objectmodel::Data<T> data;
 
@@ -48,7 +62,45 @@ void BM_Data_ReadAccessor(benchmark::State& state)
     for (auto _ : state)
     {
         auto ra = sofa::helper::getReadAccessor(data);
-        const T& t = ra; //an alternative is: const T& t = *ra;
+        const T& t = ra;
+        benchmark::DoNotOptimize(t);
+    }
+}
+
+template<class T>
+void BM_Data_ReadAccessor_ExplicitConversion(benchmark::State& state)
+{
+    sofa::core::objectmodel::Data<T> data;
+
+    if constexpr (sofa::type::trait::is_vector<T>::value)
+    {
+        auto wa = sofa::helper::getWriteOnlyAccessor(data);
+        wa.resize(state.range(0));
+    }
+
+    for (auto _ : state)
+    {
+        auto ra = sofa::helper::getReadAccessor(data);
+        const T& t = static_cast<const T&>(ra);
+        benchmark::DoNotOptimize(t);
+    }
+}
+
+template<class T>
+void BM_Data_ReadAccessor_StarOperatorConversion(benchmark::State& state)
+{
+    sofa::core::objectmodel::Data<T> data;
+
+    if constexpr (sofa::type::trait::is_vector<T>::value)
+    {
+        auto wa = sofa::helper::getWriteOnlyAccessor(data);
+        wa.resize(state.range(0));
+    }
+
+    for (auto _ : state)
+    {
+        auto ra = sofa::helper::getReadAccessor(data);
+        const T& t = *ra;
         benchmark::DoNotOptimize(t);
     }
 }
