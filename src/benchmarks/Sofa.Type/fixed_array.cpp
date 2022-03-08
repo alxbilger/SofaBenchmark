38bc2eee4ab4f2c2909388f9d0e6f8bf584f5ddb
Frederick Roy <frederick.roy@inria.fr>
2022-03-08 10:34:17 +0100

    add default ctor benches

--- src/benchmarks/Sofa.Type/fixed_array.cpp
+++ src/benchmarks/Sofa.Type/fixed_array.cpp
@@ -6,6 +6,10 @@
 #include <array>
 
 template <typename Container>
+static void BM_FixedArray_defaultconstruct(benchmark::State& state);
+static void BM_FixedArray_defaultconstruct_vec3noinit(benchmark::State& state);
+
+template <typename Container>
 static void BM_FixedArray_construct(benchmark::State& state);
 
 template <typename Container>
@@ -17,8 +21,13 @@ static void BM_FixedArray_clear(benchmark::State& state);
 using stdarray3f = std::array<float, 3>;
 using sofatypefixedarray3f = sofa::type::fixed_array<float, 3>;
 
-constexpr int64_t minSubIterations = 8 << 8;
-constexpr int64_t maxSubIterations = 8 << 10;
+constexpr int64_t minSubIterations = 8 << 10;
+constexpr int64_t maxSubIterations = 8 << 12;
+
+BENCHMARK_TEMPLATE1(BM_FixedArray_defaultconstruct, stdarray3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} })->Unit(benchmark::kMicrosecond);
+BENCHMARK_TEMPLATE1(BM_FixedArray_defaultconstruct, sofatypefixedarray3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} })->Unit(benchmark::kMicrosecond);
+BENCHMARK_TEMPLATE1(BM_FixedArray_defaultconstruct, sofa::type::Vec3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} })->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_FixedArray_defaultconstruct_vec3noinit)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} })->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_TEMPLATE1(BM_FixedArray_construct, stdarray3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} })->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE1(BM_FixedArray_construct, sofatypefixedarray3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} })->Unit(benchmark::kMicrosecond);
@@ -29,6 +38,29 @@ BENCHMARK_TEMPLATE1(BM_FixedArray_constantAssignment, sofatypefixedarray3f)->Ran
 BENCHMARK_TEMPLATE1(BM_FixedArray_constantAssignment, sofa::type::Vec3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} });
 
 BENCHMARK_TEMPLATE1(BM_FixedArray_clear, sofa::type::Vec3f)->RangeMultiplier(2)->Ranges({ {minSubIterations, maxSubIterations} });
+
+template <typename Container>
+void BM_FixedArray_defaultconstruct(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        for (unsigned int i = 0; i < state.range(0); ++i)
+        {
+            benchmark::DoNotOptimize(Container{ });
+        }
+    }
+}
+
+void BM_FixedArray_defaultconstruct_vec3noinit(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        for (unsigned int i = 0; i < state.range(0); ++i)
+        {
+            benchmark::DoNotOptimize(sofa::type::Vec3f { sofa::type::NOINIT });
+        }
+    }
+}
 
 template <typename Container>
 void BM_FixedArray_construct(benchmark::State& state)
