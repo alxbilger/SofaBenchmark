e75aba66b24b9cd17bdd4c8c5483d3b211d61135
Alex Bilger <alexbilger0@gmail.com>
2022-12-16 14:44:55 +0100

    Call cleanup to avoid dangling threads

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,24 @@
 #include <benchmark/benchmark.h>
 
-BENCHMARK_MAIN();
+#include <sofa/simulation/init.h>
+
+int main(int argc, char** argv)
+{
+    char arg0_default[]="benchmark";
+    char* args_default=arg0_default;
+    if(!argv)
+    {
+        argc=1;
+        argv=&args_default;
+    }
+    ::benchmark::Initialize(&argc, argv);
+    if(::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+
+    sofa::simulation::core::init();
+
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+
+    sofa::simulation::core::cleanup();
+    return 0;
+}
