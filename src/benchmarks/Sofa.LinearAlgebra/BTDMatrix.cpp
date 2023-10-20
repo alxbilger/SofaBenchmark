51fb5a9918bae91ccbb7e9dc5e77d69bfbc59e07
Alex Bilger <alexbilger0@gmail.com>
2023-10-20 11:07:12 +0200

    comparison with the scalar accumulation

--- src/benchmarks/Sofa.LinearAlgebra/BTDMatrix.cpp
+++ src/benchmarks/Sofa.LinearAlgebra/BTDMatrix.cpp
@@ -5,15 +5,13 @@
 
 constexpr int64_t maxSubIterations = 8 << 7;
 
-template<std::size_t N, typename T>
-static void BM_BTDMatrix_add(benchmark::State& state)
-{
-    sofa::linearalgebra::BTDMatrix<N, T> matrix;
 
+template <std::size_t N, typename T>
+static void prepareData(benchmark::State& state, sofa::linearalgebra::BTDMatrix<N, T>& matrix, std::vector<sofa::type::Mat<N, N, T>>& matricesToAdd, std::vector<std::pair<sofa::Index, sofa::Index>>& indices)
+{
     constexpr auto totalsize = maxSubIterations * N * N;
     const std::array<T, totalsize>& values = RandomValuePool<T, totalsize>::get();
 
-    std::vector<sofa::type::Mat<N, N, T>> matricesToAdd;
     matricesToAdd.resize(state.range(0));
     for (int64_t i = 0; i < state.range(0); i++)
     {
@@ -31,7 +29,6 @@ static void BM_BTDMatrix_add(benchmark::State& state)
     {
         id *= N;
     }
-    std::vector<std::pair<sofa::Index, sofa::Index>> indices;
     indices.reserve(state.range(0));
     for (int64_t i = 0; i < state.range(0) * 2; i += 2)
     {
@@ -40,6 +37,38 @@ static void BM_BTDMatrix_add(benchmark::State& state)
 
     const auto max = *std::max_element(randIndices.begin(), randIndices.end());
     matrix.resize(N * ((max+N) / N), N * ((max+N) / N));
+}
+
+template<std::size_t N, typename T>
+static void BM_BTDMatrix_add(benchmark::State& state)
+{
+    sofa::linearalgebra::BTDMatrix<N, T> matrix;
+    std::vector<sofa::type::Mat<N, N, T>> matricesToAdd;
+    std::vector<std::pair<sofa::Index, sofa::Index>> indices;
+    prepareData<N, T>(state, matrix, matricesToAdd, indices);
+
+    for (auto _ : state)
+    {
+        for (int64_t i = 0; i < state.range(0); ++i)
+        {
+            for (sofa::Index j = 0; j < N; ++j)
+            {
+                for (sofa::Index k = 0; k < N; ++k)
+                {
+                    matrix.add(indices[i].first, indices[i].second, matricesToAdd[i](j, k));
+                }
+            }
+        }
+    }
+}
+
+template<std::size_t N, typename T>
+static void BM_BTDMatrix_addBlock(benchmark::State& state)
+{
+    sofa::linearalgebra::BTDMatrix<N, T> matrix;
+    std::vector<sofa::type::Mat<N, N, T>> matricesToAdd;
+    std::vector<std::pair<sofa::Index, sofa::Index>> indices;
+    prepareData<N, T>(state, matrix, matricesToAdd, indices);
 
     for (auto _ : state)
     {
@@ -51,3 +80,4 @@ static void BM_BTDMatrix_add(benchmark::State& state)
 }
 
 BENCHMARK_TEMPLATE(BM_BTDMatrix_add, 6, SReal)->Range(8 << 3, maxSubIterations);
+BENCHMARK_TEMPLATE(BM_BTDMatrix_addBlock, 6, SReal)->Range(8 << 3, maxSubIterations);
