efb9e0c5e317f326c56ab5a09a28ac8f874581bb
Alex Bilger <alexbilger0@gmail.com>
2024-02-29 14:46:37 +0100

    Generate any type of Eigen::SparseMatrix

--- src/utils/SparseMatrix.h
+++ src/utils/SparseMatrix.h
@@ -4,17 +4,17 @@
 #include <sofa/config.h>
 
 /// Sparsity is a ratio between 0 and 1
-template <typename TReal = SReal>
-static void generateRandomSparseMatrix(Eigen::SparseMatrix<TReal>& eigenMatrix, Eigen::Index nbRows, Eigen::Index nbCols, TReal sparsity)
+template<typename _Scalar, int _Options, typename _StorageIndex>
+static void generateRandomSparseMatrix(Eigen::SparseMatrix<_Scalar, _Options, _StorageIndex>& eigenMatrix, Eigen::Index nbRows, Eigen::Index nbCols, _Scalar sparsity)
 {
     eigenMatrix.resize(nbRows, nbCols);
-    sofa::type::vector<Eigen::Triplet<TReal> > triplets;
+    sofa::type::vector<Eigen::Triplet<_Scalar> > triplets;
 
-    const auto nbNonZero = static_cast<Eigen::Index>(sparsity * static_cast<TReal>(nbRows*nbCols));
+    const auto nbNonZero = static_cast<Eigen::Index>(sparsity * static_cast<_Scalar>(nbRows*nbCols));
 
     for (Eigen::Index i = 0; i < nbNonZero; ++i)
     {
-        const auto value = static_cast<TReal>(sofa::helper::drand(1));
+        const auto value = static_cast<_Scalar>(sofa::helper::drand(1));
         const auto row = static_cast<Eigen::Index>(sofa::helper::drandpos(nbRows) - 1e-8);
         const auto col = static_cast<Eigen::Index>(sofa::helper::drandpos(nbCols) - 1e-8);
         triplets.emplace_back(row, col, value);
